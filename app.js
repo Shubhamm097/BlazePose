4ab1e5f2894881139814819ccc5e0cdb81e2eaa7
Shubham Sharma <64421924+Shubhamm097@users.noreply.github.com>
2021-06-01 16:04:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,62 @@
 const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
+let cnt = 0;
+let flag = 0;
 
 function onResults(results) {
   canvasCtx.save();
-  const c = canvasElement.getContext('2d');
-  c.fillStyle = "red";
-  c.fillRect(10, 10, 125, 700);
-  if (results.multiHandLandmarks[0][0].x < 0.20) {
-    console.log("Completed");
-    const c = canvasElement.getContext('2d');
-    c.fillStyle = "blue";
-    c.fillRect(10, 10, 125, 700);
+  if (flag == 0) {
+    flag = 1;
+    canvasCtx.beginPath();
+    canvasCtx.lineWidth = "4";
+    canvasCtx.strokeStyle = "black";
+    canvasCtx.rect(18, 12, 150, 700);
+    canvasCtx.stroke();
+    canvasCtx.fillStyle = "red";  
+    canvasCtx.fillRect(18, 12, 150, 700);
   }
-  canvasCtx.restore();
+  if (cnt == 100) {
+    alert('SMASH IT ONCE MORE!!!');
+    canvasCtx.clearRect(18, 12, 150, 700);
+    canvasCtx.beginPath();
+    canvasCtx.lineWidth = "4";
+    canvasCtx.strokeStyle = "black";
+    canvasCtx.rect(18, 12, 150, 700);
+    canvasCtx.stroke();
+    canvasCtx.fillStyle = "red";  
+    canvasCtx.fillRect(18, 12, 150, 700);
+    cnt = 0;
+    flag = 0;
+  }
+
+  if ((results.poseLandmarks[20]['x'] > 0.8 &&
+     results.poseLandmarks[20]['visibility'] > 0.85) || (results.poseLandmarks[19]['x'] > 0.8 &&
+     results.poseLandmarks[19]['visibility'] > 0.85) || (results.poseLandmarks[19]['x'] > 0.8 &&
+     results.poseLandmarks[19]['visibility'] > 0.85) || (results.poseLandmarks[17]['x'] > 0.8 &&
+     results.poseLandmarks[17]['visibility'] > 0.85) || (results.poseLandmarks[18]['x'] > 0.8 &&
+     results.poseLandmarks[18]['visibility'] > 0.85)) {
+    cnt++;
+    if (cnt <= 100) {
+      canvasCtx.clearRect(18, 12, 150, 7*cnt);
+    }
+  }
 }
 
-const hands = new Hands({locateFile: (file) => {
-  return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+const pose = new Pose({locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
 }});
-hands.setOptions({
-  maxNumHands: 2,
+pose.setOptions({
+  modelComplexity: 1,
+  smoothLandmarks: true,
   minDetectionConfidence: 0.5,
   minTrackingConfidence: 0.5
 });
-hands.onResults(onResults);
+pose.onResults(onResults);
 
 const camera = new Camera(videoElement, {
   onFrame: async () => {
-    await hands.send({image: videoElement});
+    await pose.send({image: videoElement});
   },
   width: 1280,
   height: 720
